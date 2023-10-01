2cd91f3af07221fe0ee982263475e932a6472cfe
Hamiltonmcrop <hamiltonmcrop1@gmail.com>
2023-10-01 20:07:58 +0300

    main7

--- main7.cpp
+++ main7.cpp
@@ -1,27 +1,37 @@
+//Web Billing
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-// Function to check if an account number is valid
+/**
+ * Function to check if an account number is valid
+ */
 bool is_valid_account_number(int accountNumber) {
     int sum = 0;
     int temp = accountNumber;
 
-    // Calculate the sum of digits according to the validation rule
+    /**Calculate the sum of digits according to the validation rule
+     */
     for (int i = 1; i <= 5; i++) {
         int digit = temp % 10;
         sum += digit * i;
         temp /= 10;
     }
 
-    // Check if the check digit is valid
+    /**
+     * Check if the check digit is valid
+     */
     return (sum % 10 == accountNumber / 100000);
 }
 
-// Function to calculate the customer's bill
+/**
+ * Function to calculate the customer's bill
+ */
 void calculate_bill(double baseCharge, double diskLimit, double maxUsage, double endUsage, double& overLimitSurcharge, double& endOfMonthPenaltySurcharge, double& gst, double& totalAmountDue) {
-    // Calculate the over-limit surcharge based on the provided table
+    /**
+     * Calculate the over-limit surcharge based on the provided table
+     */
     double overLimit = maxUsage - diskLimit;
     if (overLimit > 0) {
         if (overLimit <= 50.0) {
@@ -35,25 +45,35 @@ void calculate_bill(double baseCharge, double diskLimit, double maxUsage, double
         }
     }
 
-    // Calculate the end-of-month penalty surcharge
+    /**
+     * Calculate the end-of-month penalty surcharge
+     */
     if (endUsage > diskLimit) {
         endOfMonthPenaltySurcharge = max(0.05 * (endUsage - diskLimit), 5.0);
     }
 
-    // Calculate GST
+    /**
+     * Calculate GST
+     */
     gst = (baseCharge + overLimitSurcharge + endOfMonthPenaltySurcharge) * 0.05;
 
-    // Calculate total amount due
+    /**
+     * Calculate total amount due
+     */
     totalAmountDue = baseCharge + overLimitSurcharge + endOfMonthPenaltySurcharge + gst;
 }
 
-// Function to print the customer's bill
+/**
+ * Function to print the customer's bill
+ */
 void print_bill(int accountNumber, double diskLimit, double startDiskUsage, double maxDiskUsage, double endDiskUsage, double baseCharge, double overLimitSurcharge, double endOfMonthPenaltySurcharge, double gst, double totalAmountDue) {
     cout << "Account Number:\t\t" << accountNumber << endl;
+    cout << endl;
     cout << "Disk Allotment:\t\t" << fixed << setprecision(3) << diskLimit << " MB" << endl;
     cout << "Beginning of Month:\t" << fixed << setprecision(3) << startDiskUsage << " MB" << endl;
     cout << "Maximum during Month:\t" << fixed << setprecision(3) << maxDiskUsage << " MB" << endl;
     cout << "End of Month:\t\t" << fixed << setprecision(3) << endDiskUsage << " MB" << endl;
+    cout << endl;
     cout << "Base Charge:\t\t" << fixed << setprecision(2) << baseCharge << endl;
 
     if (overLimitSurcharge > 0) {
@@ -65,7 +85,7 @@ void print_bill(int accountNumber, double diskLimit, double startDiskUsage, doub
     }
 
     cout << "GST:\t\t\t" << fixed << setprecision(2) << gst << endl;
-    cout << "Amount Due:    $ \t" << fixed << setprecision(2) << totalAmountDue << endl;
+    cout << "Amount Due:     $ \t" << fixed << setprecision(2) << totalAmountDue << endl;
 }
 
 int main() {
@@ -73,13 +93,19 @@ int main() {
     double baseCharge, diskLimit, startDiskUsage, maxDiskUsage, endDiskUsage;
     char transactionFlag;
 
-    // Read the header record
+    /**
+     * Read the header record
+     */
     cin >> accountNumber >> baseCharge >> diskLimit >> startDiskUsage;
 
-    // Read the transaction record
+    /**
+     * Read the transaction record
+     */
     cin >> maxDiskUsage >> endDiskUsage >> transactionFlag;
 
-    // Check if the account number is valid
+    /**
+     * Check if the account number is valid
+     */
     if (!is_valid_account_number(accountNumber)) {
         cout << "Invalid account number " << accountNumber << " found" << endl;
         cout << "*** Account " << accountNumber << " not processed due to errors ***" << endl;
